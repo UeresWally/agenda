076b977c4e19d42227eb316ec185df588884bf14
Wallace Caetano <wallacesantoscs@gmail.com>
2021-04-13 13:36:25 -0300

    first commit

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:agenda/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Contact c = Contact();
+    c.name = "Souza Santos";
+    c.email = "souza@gmail.com";
+    c.phone = "(12) 981861601";
+    c.img = "imgtest"; 
+
+    helper.saveContact(c); 
+
+    helper.getAllContacts().then((list){
+      print(list);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
